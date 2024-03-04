f1d65d0c2f1753e9ba3f14bd0798d13953cc2301
Sadia0098 <159723541+Sadia0098@users.noreply.github.com>
2024-03-05 01:36:22 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+//Data Types
+//1. string 
+//Ex no 1.
+var firstName = "kiswa Asif";
+console.log("Have a beautifull day", " " + firstName);
+//Ex no 2.
+var user_Name = "Karachi University";
+console.log("congulration to pass MBA from", " " + user_Name);
+//2. number
+//Ex no 1.
+var finalMarks = 50;
+console.log("80% marks on final exame", " " + finalMarks);
+//Ex n0 2.
+var invite = 36;
+var capacity = 100;
+console.log("remaining capacity", capacity - invite);
+// boolean
+//Ex no 3.
+var MyName = true;
+console.log(MyName);
+//Ex no 3.
+var a = false;
+console.log(a);
+//null
+var space = null;
+console.log(space);
+//undefined
+var mobile = undefined;
+console.log(mobile);
---
+++ main.ts
@@ -0,0 +1,38 @@
+//Data Types
+//1. string 
+//Ex no 1.
+let firstName: string = "kiswa Asif";
+console.log("Have a beautifull day"," " + firstName);
+//Ex no 2.
+let user_Name: string = "Karachi University";
+console.log("congulration to pass MBA from"," " + user_Name);
+
+//2. number
+//Ex no 1.
+let finalMarks: number = 50
+console.log("80% marks on final exame", " " + finalMarks);
+//Ex n0 2.
+let invite:number = 36;
+let capacity:number = 100;
+console.log("remaining capacity", capacity-invite);
+
+// boolean
+//Ex no 3.
+let MyName:boolean= true;
+console.log(MyName);
+//Ex no 3.
+let a:boolean = false;
+console.log(a);
+
+//null
+let space:null=null;
+console.log(space);
+
+//undefined
+let mobile:undefined = undefined
+console.log(mobile);
+
+//symbol
+let mysym1 :symbol 
+
+
